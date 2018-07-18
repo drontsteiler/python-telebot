23c86c371d9b0c8015d0da991eb82bd58e3cc85d
foo <bar@example.com>
2018-07-19 01:03:27 +0300

    Error

--- flask_bot.py
+++ flask_bot.py
@@ -28,7 +28,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://ramazan-telebot-test.herokuapp.com//' + TOKEN)
+    bot.set_webhook(url='https://ramazan-telebot-test.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
