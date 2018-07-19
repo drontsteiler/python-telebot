3638aa689064640de916a23293a6f925f6f7dd84
foo <bar@example.com>
2018-07-19 17:55:44 +0300

    dcvdvad

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def weatherapp(text):
         if (temp_max > 0):
             temp_max = "+" + str(temp_max)
         weather=""
-        weather = "Сегодня в  " + name + "\n"  + icon + str(
+        weather = "Сегодня в  " + name + "\n"  + icon +"\n"+ str(
             temp) + "\n" + str(conditions).capitalize() + " от " + str(temp_min) + " до " + str(temp_max)
         return weather
     except Exception as e:
