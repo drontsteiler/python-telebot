c93276245f5950d58cb1a3b4c0c6ff749ab8fb40
foo <bar@example.com>
2018-07-21 00:05:03 +0300

    dcvsdvf

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def weatherapp(text):
                 weather = str(date) + " в  " + name + "\n" + conditions.capitalize() + "\nТемпература: " + str(
                     temp) +"\nВлажность: " + humi + "\nОблачность: " + clouds + " %\nСкорость ветра: " + wind_speed + " м/с\nIcon:<a href = '" + im + "'>.</a>\n\n"
                 print(weather)
+                break
         return weather
 
     except Exception as e:
