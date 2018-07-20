1d9fa719d473701d0cabbbd9e5b8bfec2d0127e7
foo <bar@example.com>
2018-07-21 01:31:48 +0300

    sdvdsV

---
+++ for_test.py
@@ -0,0 +1,16 @@
+gifs = {
+    '01d': 'https://i.pinimg.com/originals/12/05/1f/12051f635e87316a758c126151690519.gif',
+    '02d': 'https://i.pinimg.com/originals/7d/cb/5b/7dcb5be30b0594b7c45e285ebb01b2bf.gif',
+    '03d': 'https://i.pinimg.com/originals/4d/94/5f/4d945fdce719f03a255b5270f39546db.gif',
+    '04d': 'https://i.pinimg.com/originals/4d/94/5f/4d945fdce719f03a255b5270f39546db.gif',
+    '09d': 'https://i.pinimg.com/originals/37/8c/11/378c1143e77f70582a1d316647c923f6.gif',
+    '10d': 'https://i.pinimg.com/originals/eb/b1/f6/ebb1f6930e3fe59e4ed399ca4f5fb0b0.gif',
+    '11d': 'https://i.pinimg.com/originals/49/b8/94/49b894d42ec5fae7a8531b9f29bf4649.gif',
+    '13d': 'https://i.pinimg.com/originals/63/76/47/6376471219dbbd746ebe5fbd81e9dc4b.gif',
+    '50d': 'https://i.pinimg.com/originals/4d/94/5f/4d945fdce719f03a255b5270f39546db.gif'
+}
+
+for key, val in gifs.items():
+    if (key == '02d'):
+        print(val)
+        break
