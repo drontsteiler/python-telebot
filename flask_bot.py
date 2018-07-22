85fef3b1bc34810700c43e6d84d361e6735fdbce
foo <bar@example.com>
2018-07-22 23:43:25 +0300

    AdsdvdsvdsV

--- flask_bot.py
+++ flask_bot.py
@@ -18,15 +18,12 @@ print("_______________________________________________________________\n"
 
 @bot.message_handler(commands=["start"])
 def handle_start(message):
-    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-    user_markup.row('/start', '/stop')
-    bot.send_message(message.from_user.id, "Welcome, " + message.from_user.first_name, reply_markup=user_markup)
+    bot.send_message(message.from_user.id, "Welcome, " + message.from_user.first_name)
 
 
 @bot.message_handler(commands=["stop"])
 def handle_start(message):
-    hide_markup = telebot.types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, "..", reply_markup=hide_markup)
+    bot.send_message(message.chat.id, "..")
 
 
 @bot.message_handler(content_types="text")
